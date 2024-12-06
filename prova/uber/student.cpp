4df6cd5ad87621614d8785b723857759ffca4e8e
jeffmatt79 <112177714+jeffmatt79@users.noreply.github.com>
2024-12-06 01:08:14 +0000

    TESTE

--- prova/uber/student.cpp
+++ prova/uber/student.cpp
@@ -1,6 +1,124 @@
 #include <iostream>
 #include "fn.hpp"
+#include <iostream>
+#include <memory>
+#include <optional>
 
+class pessoa {
+private:
+    std::string nome;
+    int money;
+
+public:
+    pessoa(std::string nome = "", int money = 0) : nome(nome), money(money) {}
+
+    void salario(int money) {
+        this->money = money;
+    }
+
+    int saldo() {
+        return money;
+    }
+
+    void mudarNome(std::string nome) {
+        this->nome = nome;
+    }
+
+    std::string Getnome() {
+        return this->nome;
+    }
+
+    void show() {
+        if (this->nome == "") {
+            std::cout << "none\n";
+        } else {
+            std::cout << this->nome << ":" << this->money << "\n";
+        }
+    }
+};
+
+class moto {
+private:
+    int custo;
+    std::shared_ptr<pessoa> motorista;
+    std::shared_ptr<pessoa> passageiro;
+
+public:
+    moto() : custo(0), motorista(nullptr), passageiro(nullptr) {}
+
+    void setdriver(std::shared_ptr<pessoa> m) {
+        motorista = m;
+    }
+
+    void setpass(std::shared_ptr<pessoa> p) {
+        passageiro = p;
+    }
+
+    void drive(int km) {
+        if (!motorista) {
+            std::cout << "não tem motorista\n";
+            return;
+        }
+        this->custo += km;
+    }
+
+    void leave() {
+        if (!passageiro) {
+            std::cout << "não tem passageiro\n";
+            return;
+        }
+
+        if (passageiro->saldo() < this->custo) {
+            std::cout << "pobre\n";
+            passageiro->salario(0);
+            motorista->salario(motorista->saldo() + custo);
+            custo = 0;
+            passageiro = std::make_shared<pessoa>();
+             passageiro = std::make_shared<pessoa>();
+        } else {
+            passageiro->salario(motorista->saldo() - this->custo);
+            motorista->salario(motorista->saldo() + custo);
+            custo = 0;
+            passageiro = std::make_shared<pessoa>();
+        }
+    }
+
+    void show() {
+        std::cout << "const:" << this->custo << ", driver:";
+        if (motorista) {
+            motorista->show();
+        } else {
+            std::cout << "none\n";
+        }
+        std::cout << ", pass:";
+        if (passageiro) {
+            passageiro->show();
+        } else {
+            std::cout << "none\n";
+        }
+        std::cout << "\n";
+    }
+};
+
+int main() {
+    std::shared_ptr<pessoa> lucas = std::make_shared<pessoa>("lucas", 10000);
+    std::shared_ptr<pessoa> Marcos = std::make_shared<pessoa>("Marcos", 500);
+
+    moto motoca;
+
+    motoca.show();
+    
+    motoca.setdriver(lucas);
+    motoca.setpass(Marcos);
+    
+    motoca.show();
+    
+    motoca.drive(550);
+    motoca.show();
+    
+    motoca.leave();
+    motoca.show();
+}
 
 class Adapter {
 public:
